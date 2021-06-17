13b0010a3ab342f09d02565895324c8394d6b07e
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-17 10:33:34 -0300

    refactor: Token expiration time

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,8 +1,7 @@
 export default {
-    secret_token: `${process.env.SECRET_TOKEN}`,
-    expires_in_token: "15m",
-    secret_refresh_token: `${process.env.SECRET_REFRESH_TOKEN}`,
-    expires_in_refresh_token: "2h",
-    expires_refresh_token_days: 30,
-  };
-  
+  secret_token: `${process.env.SECRET_TOKEN}`,
+  expires_in_token: "4h",
+  secret_refresh_token: `${process.env.SECRET_REFRESH_TOKEN}`,
+  expires_in_refresh_token: "2h",
+  expires_refresh_token_days: 30,
+};
