2c0d2a35c729b134c0f1e98cf2e086ff0639f785
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-17 16:16:06 -0300

    feat: Local Storage Provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,9 +1,1 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-
-container.registerSingleton<IDateProvider>(
-  "DayjsDateProvider",
-  DayjsDateProvider,
-);
+import "./StorageProvider";
