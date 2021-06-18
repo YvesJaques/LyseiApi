fde317f211c1fc8edd5c0a150ea052e70fe045ea
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-18 14:45:56 -0300

    feat: Get user profile

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
 import { Router } from "express";
+
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 
 export { usersRoutes };
