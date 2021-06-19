4363dcb137388997503d4ecdb8afe073ce4f7160
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-19 13:29:43 -0300

    refactor: refresh_token variable name

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -5,16 +5,16 @@ import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
 
 class RefreshTokenController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const token =
-      request.body.token ||
-      request.headers["x-access-token"] ||
-      request.query.token;
+    const refresh_token =
+      request.body.refresh_token ||
+      request.headers["x-access-refresh_token"] ||
+      request.query.refresh_token;
 
     const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
 
-    const refresh_token = await refreshTokenUseCase.execute(token);
+    const new_refresh_token = await refreshTokenUseCase.execute(refresh_token);
 
-    return response.json(refresh_token);
+    return response.json(new_refresh_token);
   }
 }
 
