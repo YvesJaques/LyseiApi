fcd3dde02a040ebd79a6fd685e790934c06b1e1a
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-20 00:58:21 -0300

    feat: Rate Limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -9,14 +9,19 @@ import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
+import rateLimiter from "./middlewares/rateLimiter";
 import { router } from "./routes";
 
 createConnection();
 const app = express();
 
+app.use(rateLimiter);
+
 app.use(express.json());
 
+
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+
 app.use(cors());
 app.use(router);
 
