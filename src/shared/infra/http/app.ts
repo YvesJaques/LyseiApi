fa16a735a26c9070c3f67b9c10fb9563badadb63
Yves Morais Jaques <ymoraisjaques@gmail.com>
2021-06-20 00:33:10 -0300

    Cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import "dotenv/config";
 import upload from "@config/upload";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 
@@ -16,6 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
+app.use(cors());
 app.use(router);
 
 app.use(
